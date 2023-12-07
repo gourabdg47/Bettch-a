a9aeafbc2511ce718ab45e8ae14c167a7a248cbd
Gourab Dasgupta <gourabofficial26@gmail.com>
2023-12-07 13:55:17 +0000

    CLI with click implimented but not working, cmd: pip install -e . , prev commit cli package was missing

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='Bettch-CLI',
+    version='1.0',
+    packages=['cli', 'cli.commands'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bettch=cli.cli:cli',
+        ],
+    },
+
+)
